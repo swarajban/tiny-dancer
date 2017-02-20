900195c8b7353281a3237e847505e017640e4bb5
Swaraj Banerjee <swaraj@semprehealth.com>
2017-02-19 18:25:47 -0800

    Finished script to save only new shows to db

--- updateShows.js
+++ updateShows.js
@@ -1,6 +1,9 @@
 const jsdom = require('jsdom')
 const Promise = require('bluebird')
 const moment = require('moment')
+const md5 = require('md5')
+
+const { pg, SHOWS } = require('./knex')
 const { PG_DATE_FMT } = require('./lib')
 
 const EDM_DATE_FMT = 'MM.DD.YYYY'
@@ -42,10 +45,13 @@ function parseShows (window) {
         return
       }
       const [dateString, _, artists, venue] = showString.split('•').map((string) => string.trim())
+      const show_date = moment(dateString, EDM_DATE_FMT).format(PG_DATE_FMT)
+      const show_hash = md5(`${artists}${venue}${show_date}`)
       shows.push({
-        date: moment(dateString, EDM_DATE_FMT).format(PG_DATE_FMT),
         artists,
-        venue
+        venue,
+        show_date,
+        show_hash
       })
     }
   )
@@ -54,7 +60,51 @@ function parseShows (window) {
 }
 
 function saveShows (shows) {
-  console.log(shows)
+  const show_hashes = shows.map((show) => show.show_hash)
+  console.log('Fetching existing show hashes...')
+  return pg(SHOWS)
+    .whereIn('show_hash', show_hashes)
+    .pluck('show_hash')
+    .then(
+      (existingHashList) => {
+        console.log(`Found ${existingHashList.length}`)
+        const existingHashSet = new Set(existingHashList)
+        const newShows = shows
+          .filter(
+            (show) => {
+              return ! existingHashSet.has(show.show_hash)
+            }
+          )
+          .map(
+            (show) => {
+              delete show.show_hash
+              show.legitness = 2
+              return show
+            }
+          )
+
+        const numNewShows = newShows.length
+        if (numNewShows > 0) {
+          console.log(`Saving ${newShows.length} shows`)
+          return pg(SHOWS).insert(newShows)
+        } else {
+          console.log(`No new shows found!`)
+          return { rowCount: 0}
+        }
+
+      }
+    )
+    .then(
+      ({ rowCount }) => {
+        console.log(`Successfully saved ${rowCount} shows!`)
+      }
+    )
+    .catch(
+      (err) => {
+        console.log(`Error saving shows`, err)
+        throw err
+      }
+    )
 }
 
 module.exports = {
